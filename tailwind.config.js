55a3f15acd804bb1cd40c73e4f20f3d777f3f88b
구정은 <jemilykoo@gmail.com>
2025-05-27 16:03:39 +0900

    feat: 테마 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
